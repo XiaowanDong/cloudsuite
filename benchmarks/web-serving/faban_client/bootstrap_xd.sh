c126372f8ee79ea071aeec8745d42a8fb908d239
XiaowanDong <xdong@cs.rochester.com>
2019-07-03 04:47:06 -0500

    Added more files that set up the environment on native machines instead of Docker

---
+++ benchmarks/web-serving/faban_client/bootstrap_xd.sh
@@ -0,0 +1,59 @@
+#!/usr/local/bin/bash
+
+if [ $# -gt 2 ]; then
+  echo "Usage: faban_client WEB_SERVER_IP [LOAD_SCALE]"
+  exit 1
+fi
+
+if [ $# -lt 1 ]; then
+  echo "Web server IP is a mandatory parameter."
+  exit 1
+fi
+
+WEB_SERVER_IP=$1
+LOAD_SCALE=${2:-7}
+
+export JAVA_HOME=/usr/local/openjdk8
+export FABAN_HOME=/faban
+echo "Load scale is ${LOAD_SCALE}"
+
+while [ "$(curl -sSI ${WEB_SERVER_IP}:8080 | grep 'HTTP/1.1' | awk '{print $2}')" != "200" ]; do
+  sleep 1
+done
+
+sed -i "s@<javaHome>.*@<javaHome>$JAVA_HOME<\\/javaHome>@" /web20_benchmark/deploy/run.xml
+sed -i "s/num_users=[0-9]*$/num_users=${LOAD_SCALE}/" $FABAN_HOME/usersetup.properties
+
+$FABAN_HOME/master/bin/startup.sh
+cd /web20_benchmark/build && java -jar Usergen.jar http://${WEB_SERVER_IP}:8080
+sed -i "s/<fa:scale.*/<fa:scale>${LOAD_SCALE}<\\/fa:scale>/" /web20_benchmark/deploy/run.xml
+sed -i "s/<fa:rampUp.*/<fa:rampUp>10<\\/fa:rampUp>/" /web20_benchmark/deploy/run.xml
+sed -i "s/<fa:rampDown.*/<fa:rampDown>10<\\/fa:rampDown>/" /web20_benchmark/deploy/run.xml
+sed -i "s/<fa:steadyState.*/<fa:steadyState>30<\\/fa:steadyState>/" /web20_benchmark/deploy/run.xml
+sed -i "s/<host.*/<host>${WEB_SERVER_IP}<\\/host>/" /web20_benchmark/deploy/run.xml
+sed -i "s/<port.*/<port>8080<\\/port>/" /web20_benchmark/deploy/run.xml
+sed -i "s@<outputDir.*@<outputDir>${FABAN_HOME}\/output<\\/outputDir>@" /web20_benchmark/deploy/run.xml
+cd /web20_benchmark && ant run 
+cat $FABAN_HOME/output/*/summary.xml
+
+
+#while [ "$(curl  -sSI ${WEB_SERVER_IP}:8080 | grep 'HTTP/1.1' | awk '{print $2}')" != "200" ]; do
+#	echo "Could not perform HTTP 200 GET from: ${WEB_SERVER_IP}:8080"
+#      	sleep 2
+#done
+#
+#echo "Finally done with the loop!!!"
+#sed -i "" -e"s/num_users=500/num_users=${LOAD_SCALE}/g" /faban/usersetup.properties
+#echo $JAVA_HOME
+#/faban/master/bin/startup.sh
+#cd /web20_benchmark/build && java -jar Usergen.jar http://${WEB_SERVER_IP}:8080
+#sed -i "" "s/<fa:scale.*/<fa:scale>${LOAD_SCALE}<\\/fa:scale>/g" /web20_benchmark/deploy/run.xml
+#sed -i "" "s/<fa:rampUp.*/<fa:rampUp>10<\\/fa:rampUp>/g" /web20_benchmark/deploy/run.xml
+#sed -i "" "s/<fa:rampDown.*/<fa:rampDown>10<\\/fa:rampDown>/g" /web20_benchmark/deploy/run.xml
+#sed -i "" "s/<fa:steadyState.*/<fa:steadyState>30<\\/fa:steadyState>/g" /web20_benchmark/deploy/run.xml
+#sed -i "" "s/<host.*/<host>${WEB_SERVER_IP}<\\/host>/g" /web20_benchmark/deploy/run.xml
+#sed -i "" "s/<port.*/<port>8080<\\/port>/g" /web20_benchmark/deploy/run.xml
+#sed -i "" "s/<outputDir.*/<outputDir>\/faban\/output<\\/outputDir>/g" /web20_benchmark/deploy/run.xml
+#cd /web20_benchmark && ant run
+#cat /faban/output/*/summary.xml
+
