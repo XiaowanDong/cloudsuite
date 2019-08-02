99d269e7fd70265bb9d11d1a762ce8e196c2b5fd
XiaowanDong <xdong@cs.rochester.com>
2019-08-02 07:05:14 -0500

    Updated changes to make progress in setting up nginx and elgg

--- benchmarks/web-serving/web_server/set_up.sh
+++ benchmarks/web-serving/web_server/set_up.sh
@@ -29,29 +29,29 @@ make install BATCH=yes >& make_log.txt
 
 cat files/limits.conf.append >> /etc/security/limits.conf  
 
-set ELGG_VERSION=2.3.8
+export ELGG_VERSION="2.3.8"
 
 fetch https://elgg.org/download/elgg-$ELGG_VERSION.zip \
     && unzip elgg-$ELGG_VERSION.zip \
-    && mv elgg-$ELGG_VERSION /usr/share/nginx/html/elgg \
-    && chown -R 777 /usr/share/nginx/html/elgg
+    && mv elgg-$ELGG_VERSION /usr/local/share/nginx/html/elgg \
+    && chown -R 777 /usr/local/share/nginx/html/elgg
 
 
-mkdir -p /usr/share/nginx/html/
-#cp -r files/elgg_installation /usr/local/share/nginx/html/elgg
 
-
-mkdir -p /usr/local/share/nginx/html/elgg/engine/
+mkdir -p /usr/local/share/nginx/html/elgg/elgg-config
 cp files/settings.php /usr/local/share/nginx/html/elgg/elgg-config/settings.php
 
 mkdir /elgg_data
 chmod a+rw /elgg_data
 
-mkdir -p /usr/local/etc/nginx/sites-available/
-cat files/nginx_sites_avail.append >> /etc/nginx/sites-available/default
+cp /home/xd10/cloudsuite_new/cloudsuite/benchmarks/web-serving/web_server/nginx.conf /usr/local/etc/nginx/nginx.conf
+# I have modified two places in the original nginx.conf
+#(1)change the path in files/nginx_sites_avail.append to be "root /usr/local/share/nginx/html/elgg;" 
+#instead of "root /usr/share/nginx/html/elgg;"
+#(2)append the conent of files/nginx_sites_avail.append to /usr/local/etc/nginx/nginx.conf 
+#before the very last "}"
 
-#put the content of files/nginx_sites_avail.append before "}" in 
-#/usr/local/etc/nginx/nginx.conf
+
 
 
 # Append nginx_enable="YES" to /etc/rc.conf
@@ -62,4 +62,4 @@ mkdir -p /var/run/php
 service php-fpm restart
 
 #modify the path to bash in the first line of bootstrap.sh 
-./bootstrap_xd.sh -d 127.0.0.1 127.0.0.1 80
+./bootstrap.sh -d 127.0.0.1 127.0.0.1 80
